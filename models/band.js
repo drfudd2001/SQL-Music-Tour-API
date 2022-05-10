01c221bb35e81f3950c160766f576aa6fcb93d71
drfudd2001 <elmer.figueroagarcia@gmail.com>
2022-05-09 19:39:32 -0500

    Updates

--- models/band.js
+++ models/band.js
@@ -1,5 +1,5 @@
-'use strict';
-const {  Model } = require('sequelize');
+'use strict'
+const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Band extends Model {
@@ -8,11 +8,17 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate ({ MeetGreet }) {
-      // define association here
-      Band.hasMany(MetGreet, {
+    static associate({ MeetGreet, SetTime }) {
+      // meet and greets
+      Band.hasMany(MeetGreet, {
         foreignKey: "band_id",
         as: "meet_greets"
+      })
+
+      // set times 
+      Band.hasMany(SetTime, {
+        foreignKey: "band_id",
+        set_times: "set_times"
       })
     }
   }
@@ -43,6 +49,6 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'Band',
     tableName: 'bands',
     timestamps: false
-  }); 
-  return Band;
-};
+  })
+  return Band
+}
--- models/meetgreet.js
+++ models/meetgreet.js
@@ -1,5 +1,5 @@
 'use strict'
-const { Model } = require('sequelize');
+const { Model } = require('sequelize')
 
 module.exports = (sequelize, DataTypes) => {
   class MeetGreet extends Model {
@@ -8,11 +8,17 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Band }) {
-      // define association here
+    static associate({ Band, Event }) {
+      // band
       MeetGreet.belongsTo(Band, {
         foreignKey: "band_id",
         as: "band"
+      })
+
+      // event
+      MeetGreet.belongsTo(Event, {
+        foreignKey: "event_id",
+        as: "event"
       })
     }
   }
