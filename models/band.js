ca8af04b8b64567578c51b6981165b1e2fa00bc8
drfudd2001 <elmer.figueroagarcia@gmail.com>
2022-05-09 19:12:13 -0500

    Models and Associations

--- models/band.js
+++ models/band.js
@@ -1,7 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const {  Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Band extends Model {
     /**
@@ -9,8 +8,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate ({ MeetGreet }) {
       // define association here
+      Band.hasMany(MetGreet, {
+        foreignKey: "band_id",
+        as: "meet_greets"
+      })
     }
   }
   Band.init({
